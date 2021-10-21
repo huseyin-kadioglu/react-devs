d8eaabdfde191ebd4c6fec696a85dd9c184acdc1
huseyin.kadioglu <huseyin.kadioglu@linktera.com.tr>
2021-10-21 20:05:04 +0300

    adding new componenets

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,17 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../util/Card";
+import {useState} from "react";
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
+
+    const changeTitleHandler = () => {
+            setTitle("updatedTitle");
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
 
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
+            <button onClick={changeTitleHandler}> Change Title</button>
         </Card>
     );
 };
