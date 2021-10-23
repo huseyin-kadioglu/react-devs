d9ec6258ada9bfbd2ef20e7399392e3fa887826f
huseyin <huseyinavnikadioglu@gmail.com>
2021-10-23 05:33:00 +0300

    multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -37,20 +37,49 @@ const ExpenseForm = () => {
         setDate(event.target.value);
     };
 
+    const submitHandler = (event) => {
+        // default browser behavior--> page reloads.. sends a request. thats not we want.. manually collect.. we can disable deafult behavior.
+        event.preventDefault();
+
+        const expenseData = {
+            title: title,
+            amount: amount,
+            date: new Date(date)
+        }
+
+        console.log(expenseData);
+        setTitle('');
+        setAmount('');
+        setDate('');
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label> Title </label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label> Amount </label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        value={amount}
+                        onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label> Date </label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                    <input
+                        type="date"
+                        min="2019-01-01"
+                        max="2022-12-31"
+                        value={date}
+                        onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
